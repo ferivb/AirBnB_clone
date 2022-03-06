0fef07d8542bbb25aed3274f6c2143b7c636f075
SergioBalca <3896@holbertonschool.com>
2022-03-06 08:28:46 -0500

    test_base_model with to_dict() method tested addded

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-
+"""Unittests for BaseModel class"""
+from unittest import result
 from models.base_model import BaseModel
 import unittest
 from datetime import datetime
 from models.engine.file_storage import FileStorage
 import json
+import os
 
 """module that contains unittests for BaseModel class"""
 
@@ -12,27 +14,49 @@ import json
 
 
 class TestBaseModel(unittest.TestCase):
-    
+
+    def setUp(self):
+        """Metod to create an instance for each test"""
+        self.my_model = BaseModel()
+        self.my_model.save()
+
+    def tearDown(self):
+        """Method to delete file.json afeter each test"""
+        try:
+            os.remove("file.json")
+            del self.my_model
+        except Exception:
+            pass
+
     def test_save(self):
         """Test for save method"""
-
-        my_model = BaseModel()
         test_dict = {}
-        test_dict["id"] = my_model.id
-        created_at = my_model.created_at.isoformat()
-        updated_at = my_model.updated_at.isoformat()
+        test_dict["id"] = self.my_model.id
+        created_at = self.my_model.created_at.isoformat()
+        updated_at = self.my_model.updated_at.isoformat()
         test_dict["created_at"] = created_at
         test_dict["updated_at"] = updated_at
         test_dict["__class__"] = "BaseModel"
-        key = "{}.{}".format(BaseModel.__name__, my_model.id)
+        key = "{}.{}".format(BaseModel.__name__, self.my_model.id)
         result_dict = {}
         result_dict = {key: test_dict}
-        my_model.save()
+
         try:
             with open("file.json", 'r', encoding='utf-8') as file:
                 diction = json.loads(file.read())
         except Exception:
-                pass
-        
-        
+            pass
+
         self.assertEqual(result_dict, diction)
+
+    def test_to_dict(self):
+        """Test for to_dict method"""
+        test_dict = {}
+        test_dict["id"] = self.my_model.id
+        created_at = self.my_model.created_at.isoformat()
+        updated_at = self.my_model.updated_at.isoformat()
+        test_dict["created_at"] = created_at
+        test_dict["updated_at"] = updated_at
+        test_dict["__class__"] = "BaseModel"
+
+        self.assertEqual(test_dict, self.my_model.to_dict())
